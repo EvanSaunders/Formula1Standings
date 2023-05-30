999cf0cc52d8b30733eb3009fa56dcb07baa6c46
EvanSaunders <evansaund@gmail.com>
2023-05-30 18:08:40 -0500

    Added the beginnings of Spring Framework
    It seems the F1 api for standings is broken at the moment. Do not think its on my end

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,28 @@
 package org.example;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-
+@SpringBootApplication
+@RestController
 public class Main {
 
     public static void main(String[] args) throws UnirestException, ParserConfigurationException, IOException {
+        SpringApplication.run(Main.class, args);
     Grid grid = new Grid();
     System.out.println(grid);
    }
+
+   @GetMapping("/")
+   public String hello(){
+        return "Hello World";
+   }
+
 
 }
 
