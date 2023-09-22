a53b381c910dab5f92314ccd187f0bc5c3c176ec
EvanSaunders <evansaund@gmail.com>
2023-09-22 16:51:44 -0500

    Added html for yearAndDriver
    fixed spacing in table, made default values for submit buttons

--- src/main/java/org/example/Driver/DriverController.java
+++ src/main/java/org/example/Driver/DriverController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.time.Year;
 
 @RestController
 @RequestMapping(path = "/standings")
@@ -29,13 +30,16 @@ public class DriverController {
 @GetMapping(path = "/displayStandingsByYear")
 @ResponseBody
 public String printGrid(@RequestParam("year") String year) throws UnirestException, ParserConfigurationException, IOException {
-    return driverService.generateDriverStatsHTML(year);
+    if(year==""){
+        return driverService.generateDisplayByYearHTML("" + Year.now().getValue());
+    }
+    return driverService.generateDisplayByYearHTML(year);
 }
 
     @GetMapping(path = "/displayStandingsByYearAndDriver")
     public String printDriverInYear(@RequestParam("year") String year, @RequestParam("driverId") String driverId) throws UnirestException, ParserConfigurationException, IOException {
 
-        return driverService.printDriverInYear(year, driverId);
+        return driverService.generateDisplayByYearAndDriverHTML(year, driverId);
     }
 
 }
--- src/main/java/org/example/Driver/DriverService.java
+++ src/main/java/org/example/Driver/DriverService.java
@@ -15,7 +15,7 @@ public class DriverService {
         return grid.toString();
     }
     // takes the year and gets the drivers and puts formats them into an html page then returns it to DriverController as a string
-    public String generateDriverStatsHTML(String year) throws UnirestException, ParserConfigurationException, IOException {
+    public String generateDisplayByYearHTML(String year) throws UnirestException, ParserConfigurationException, IOException {
         // Create a Grid object and fetch driver information
         Grid grid = new Grid(year);
         List<Driver> drivers = grid.getDrivers(); // Assuming you have a method to get the list of drivers
@@ -28,17 +28,27 @@ public class DriverService {
                 "<html>\n" +
                 "<head>\n" +
                 "    <title>F1 Driver Stats</title>\n" +
+                "    <style>\n" +
+                "        table {\n" +
+                "            width: 100%; /* Make the table span the full width of the container */\n" +
+                "            border-collapse: collapse; /* Remove spacing between table cells */\n" +
+                "        }\n" +
+                "        th, td {\n" +
+                "            padding: 8px; /* Add padding to each cell for spacing */\n" +
+                "            text-align: center; /* Center-align table data */\n" +
+                "        }\n" +
+                "    </style>\n" +
                 "</head>\n" +
                 "<body>\n" +
                 "<h1>F1 Driver Stats for Year: [YEAR]</h1>\n" +
                 "<table>\n" +
                 "    <tr>\n" +
-                "        <th>Driver ID</th>\n" +
+                "        <th>First</th>\n" +
+                "        <th>Last</th>\n" +
                 "        <th>Code</th>\n" +
-                "        <th>Driver Number</th>\n" +
-                "        <th>Given Name</th>\n" +
-                "        <th>Family Name</th>\n" +
-                "        <th>Date Of Birth</th>\n" +
+                "        <th>Number</th>\n" +
+                "        <th>ID</th>\n" +
+                "        <th>Birthday</th>\n" +
                 "        <th>Nationality</th>\n" +
                 "        <th>Position</th>\n" +
                 "        <th>Points</th>\n" +
@@ -57,11 +67,11 @@ public class DriverService {
         StringBuilder driverRows = new StringBuilder();
         for (Driver driver : drivers) {
             String row = "<tr>" +
-                    "<td>" + driver.getId() + "</td>" +
-                    "<td>" + driver.getCode() + "</td>" +
-                    "<td>" + driver.getPermanentNumber() + "</td>" +
                     "<td>" + driver.getGivenName() + "</td>" +
                     "<td>" + driver.getFamilyName() + "</td>" +
+                    "<td>" + driver.getCode() + "</td>" +
+                    "<td>" + driver.getPermanentNumber() + "</td>" +
+                    "<td>" + driver.getId() + "</td>" +
                     "<td>" + driver.getDateOfBirth() + "</td>" +
                     "<td>" + driver.getNationality() + "</td>" +
                     "<td>" + driver.getPosition() + "</td>" +
@@ -79,11 +89,84 @@ public class DriverService {
 
         return htmlBuilder.toString();
     }
-    public String printDriverInYear(String year, String driverId) throws UnirestException, ParserConfigurationException, IOException {
+    public String generateDisplayByYearAndDriverHTML(String year, String driverId) throws UnirestException, ParserConfigurationException, IOException {
+        // Create a Grid object and fetch driver information
         Grid grid = new Grid(year);
         Driver driver = grid.getDriver(driverId);
 
-        return driver.toString();
+        // Create an HTML StringBuilder to build the HTML content
+        StringBuilder htmlBuilder = new StringBuilder();
+
+        // Load the HTML template
+        String template = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <title>F1 Driver Stats</title>\n" +
+                "    <style>\n" +
+                "        table {\n" +
+                "            width: 100%; /* Make the table span the full width of the container */\n" +
+                "            border-collapse: collapse; /* Remove spacing between table cells */\n" +
+                "        }\n" +
+                "        th, td {\n" +
+                "            padding: 8px; /* Add padding to each cell for spacing */\n" +
+                "            text-align: center; /* Center-align table data */\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<h1>F1 Driver Stats for [familyName]</h1>\n" +
+                "<table>\n" +
+                "    <tr>\n" +
+                "        <th>First</th>\n" +
+                "        <th>Last</th>\n" +
+                "        <th>Code</th>\n" +
+                "        <th>Number</th>\n" +
+                "        <th>ID</th>\n" +
+                "        <th>Birthday</th>\n" +
+                "        <th>Nationality</th>\n" +
+                "        <th>Position</th>\n" +
+                "        <th>Points</th>\n" +
+                "        <th>Wins</th>\n" +
+                "        <th>Constructor</th>\n" +
+                "        <th>Constructor Nationality</th>\n" +
+                "    </tr>\n" +
+                "    [DRIVER_DATA]\n" +
+                "</table>\n" +
+                "</body>\n" +
+                "</html>\n";
+
+        // Replace placeholders with actual data
+        template = template.replace("[familyName]", driver.getGivenName() + " " +driver.getFamilyName()+" in year "+ year);
+
+        StringBuilder driverRow = new StringBuilder();
+            String row = "<tr>" +
+                    "<td>" + driver.getGivenName() + "</td>" +
+                    "<td>" + driver.getFamilyName() + "</td>" +
+                    "<td>" + driver.getCode() + "</td>" +
+                    "<td>" + driver.getPermanentNumber() + "</td>" +
+                    "<td>" + driver.getId() + "</td>" +
+                    "<td>" + driver.getDateOfBirth() + "</td>" +
+                    "<td>" + driver.getNationality() + "</td>" +
+                    "<td>" + driver.getPosition() + "</td>" +
+                    "<td>" + driver.getPoints() + "</td>" +
+                    "<td>" + driver.getWins() + "</td>" +
+                    "<td>" + driver.getConstructor() + "</td>" +
+                    "<td>" + driver.getConstructorNationality() + "</td>" +
+                    "</tr>";
+            driverRow.append(row);
+
+        template = template.replace("[DRIVER_DATA]", driverRow.toString());
+
+        // Set the final HTML content
+        htmlBuilder.append(template);
+
+        return htmlBuilder.toString();
+
+
+        //Grid grid = new Grid(year);
+       // Driver driver = grid.getDriver(driverId);
+
+        //return driver.toString();
     }
 
 
